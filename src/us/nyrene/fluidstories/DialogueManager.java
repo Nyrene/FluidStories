bbffed475e1cd4ac8ca18fe8b8e8cd1d4e8dae76
Rachael Little <realrachael@gmail.com>
2019-07-19 11:50:14 -0400

    adding gitignore

--- src/us/nyrene/fluidstories/DialogueManager.java
+++ src/us/nyrene/fluidstories/DialogueManager.java
@@ -3,7 +3,7 @@ import java.util.UUID;
 import java.util.HashMap;
 
 public class DialogueManager {
-
+    private HashMap<String, DialogueTree> editingDialogues = new HashMap<String, DialogueTree>();
 
 
     public String saveDialogue(DialogueTree givenDialogue) {
@@ -14,7 +14,16 @@ public class DialogueManager {
         return "";
     }
 
-    public String createNewDialogue() {
+    public String closeDialogue(UUID playerID) {
+
+    }
+
+    public String createNewDialogue(String newName, UUID playerID) {
+        // file format will be 1 file per player. Top: list of player's trees
+        // then a JSON item for each dialogue
+
+        // check for duplicate name
+        // format of file name will be UUID
 
 
         return "";
